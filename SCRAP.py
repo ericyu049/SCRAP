172920e19ecd04fa69666a09a1eddd50277a8d1e
Eric <ericyu049@gmail.com>
2023-04-19 13:49:03 -0400

    flash

--- SCRAP.py
+++ SCRAP.py
@@ -52,11 +52,8 @@ three_prime_barcode = ''
 
 def fastqc():
     print('Start running FastQC -- ', datetime.now())
-    try:
+    if not os.path.exists(fastqc_report_folder):
         os.mkdir(fastqc_report_folder)
-    except:
-        print('FastQC_Reports folder already exists.')
-
     with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
         futures = [executor.submit(
             fastqc_worker, directory, sample) for sample in samples]
@@ -67,26 +64,24 @@ def fastqc():
 
 def fastqc_worker(directory, sample):
     print('Running FastQC - Sample: ', sample, ' -- ', datetime.now())
-    files = ' '.join(glob.glob(os.path.join(directory, sample) + '/*fastq*'))
+    files = ' '.join(glob.glob(os.path.join(directory, sample, '*fastq*')))
     subprocess.run(['fastqc', '-o', fastqc_report_folder, files, '-t', '2'])
     print('Done running FastQC - Sample: ', sample, ' -- ', datetime.now())
 
 
 def multiqc():
     print('Running MultiQC -- ', datetime.now())
-    try:
+    if not os.path.exists(multiqc_report_folder):
         os.mkdir(multiqc_report_folder)
-    except:
-        print('MultiQC_Report folder already exists.')
-    # subprocess.run(['multiqc', fastqc_report_folder,
-    #                '-o', multiqc_report_folder])
+    subprocess.run(['multiqc', fastqc_report_folder,
+                   '-o', multiqc_report_folder])
 
     print('Done running MultiQC -- ', datetime.now())
 
 
-def sampleWorker(df, directory, sample):
-    countReads(directory, sample)
-    if (paired_end == 'yes'):
+def sampleWorker(directory, sample, flags):
+    # countReads(directory, sample)
+    if (flags[0] == 'yes'):
         flash(directory, sample)
     # if five_prime_adapter:
     #     print('five prime adapter')
@@ -96,11 +91,12 @@ def sampleWorker(df, directory, sample):
     #     print('five prime barcode')
     # if three_prime_barcode:
     #     print('three prime barcode')
+    
 
 
 def readAdapterFile(df, sample):
     print('Extract adapter and barcode sequences from adapter file')
-
+    global five_prime_adapter, three_prime_adapter, five_prime_barcode, three_prime_barcode
     five_prime_adapter = df["5'Adapter"][sample]
     three_prime_adapter = df["3'Adapter"][sample]
     five_prime_barcode = df["5'Barcode"][sample]
@@ -108,6 +104,7 @@ def readAdapterFile(df, sample):
 
     sample_summary_txt = os.path.join(
         directory, sample, sample + '.summary.txt')
+    print(sample_summary_txt)
     if os.path.exists(sample_summary_txt):
         os.remove(sample_summary_txt)
 
@@ -123,7 +120,7 @@ def readAdapterFile(df, sample):
 
 
 def countReads(directory, sample):
-    files = glob.glob(os.path.join(directory, sample) + '/*fastq*')
+    files = glob.glob(os.path.join(directory, sample, '*fastq*'))
 
     # Separate R1 and R2 into 2 subproccess workers for counting reads.
 
@@ -148,17 +145,34 @@ def countReadsSubWorker(zipfile, directory, sample):
 
 def flash(directory, sample):
     print('Running FLASH -- ', datetime.now())
-    flash_path = os.path.join(directory, sample, '/'+sample+'_FLASH')
-    try:
+
+    flash_path = os.path.join(directory, sample, sample+'_FLASH')
+    if not os.path.exists(flash_path):
         os.mkdir(flash_path)
-    except:
-        print('Flash folder already exists')
+
     r1 = os.path.join(directory, sample, sample+'_R1.fastq.gz')
     r2 = os.path.join(directory, sample, sample+'_R2.fastq.gz')
-    flash_log = os.path.join(flash_path, '/FLASH_'+sample+'.log')
+    flash_log = os.path.join(flash_path, 'FLASH_' + sample+'.log')
+
     subprocess.run(['flash', '--allow-outies', '--output-directory='+flash_path+'/',
                    '--output-prefix='+sample, '--max-overlap=150', '--min-overlap=6', '--compress', r1, r2, '2>&1 | tee', flash_log])
+    os.rename(os.path.join(flash_path, sample+'.extendedFrags.fasq.gz'), os.path.join(directory, sample, sample+'.fastq.gz'))
 
+    if os.path.exists(flash_path):
+        os.remove(flash_path)
+
+    print('Counting number of reads from FLASH output')
+
+    zipfile = os.path.join(directory, sample, sample+'.fastq.gz')
+
+    with gzip.open(zipfile, 'rt') as f_in:
+        num_reads = sum(1 for line in f_in) // 4
+
+    sample_summary_txt = os.path.join(
+        directory, sample, sample + '.summary.txt')
+
+    with open(sample_summary_txt, 'a') as file:
+        file.write(f"{sample} combined paired-end reads: {num_reads}\n")
 
 ########## Pipeline begins here #############
 if __name__ == '__main__':
@@ -219,10 +233,14 @@ if __name__ == '__main__':
     # fastqc()
     # multiqc()
 
+
     for sample in samples:
         readAdapterFile(df, sample)
 
+
+    flags = [paired_end, pre_filtered, five_prime_adapter, three_prime_adapter, five_prime_barcode, three_prime_barcode ]
+
     with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
         futures = [executor.submit(
-            sampleWorker, df, directory, sample) for sample in samples]
+            sampleWorker, directory, sample, flags) for sample in samples]
         concurrent.futures.wait(futures)
