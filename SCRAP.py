0d9b7c2817989e51f44ed25045310ebf99b863eb
Eric <ericyu049@gmail.com>
2023-04-23 20:58:47 -0400

    debug

--- SCRAP.py
+++ SCRAP.py
@@ -151,9 +151,11 @@ def flash(directory, sample):
     r1 = os.path.join(directory, sample, f"{sample}_R1.fastq.gz")
     r2 = os.path.join(directory, sample, f"{sample}_R2.fastq.gz")
     flash_log = os.path.join(flash_path, f"FLASH_{sample}.log")
-
-    subprocess.run(['flash', '--allow-outies', '--output-directory='+flash_path+'/',
-                   '--output-prefix='+sample, '--max-overlap=150', '--min-overlap=6', '--compress', r1, r2, '2>&1 | tee', flash_log])
+    command_array = ['flash', '--allow-outies', '--output-directory='+flash_path+'/',
+                   '--output-prefix='+sample, '--max-overlap=150', '--min-overlap=6', '--compress', r1, r2, '2>&1 | tee', flash_log]
+    print(command_array)
+    # subprocess.run(['flash', '--allow-outies', '--output-directory='+flash_path+'/',
+    #                '--output-prefix='+sample, '--max-overlap=150', '--min-overlap=6', '--compress', r1, r2, '2>&1 | tee', flash_log])
     os.rename(os.path.join(flash_path, f"{sample}.extendedFrags.fasq.gz"),
               os.path.join(directory, sample, f"{sample}.fastq.gz"))
 
@@ -216,9 +218,9 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if args.help:
-        parser.print_help()
-        sys.exit()
+    # if args.help:
+    #     parser.print_help()
+    #     sys.exit()
 
     directory = args.directory
     adapter_file = args.adapter_file
